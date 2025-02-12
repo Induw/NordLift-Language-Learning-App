d2986da76f1d9e4da8606641ae4f805ae208f659
Ravindu Hettiarachchi <ravindu.hettiarachchi@ifs.com>
2025-02-12 22:50:05 +0530

    Adding multi-langugage capability to Translation Page

--- LanguageApp/Views/TypingPage.xaml.cs
+++ LanguageApp/Views/TypingPage.xaml.cs
@@ -6,7 +6,7 @@ namespace LanguageApp.Views
 {
     public partial class TypingPage : ContentPage
     {
-        private readonly Dictionary<string, string> WordTranslations = new()
+        private readonly Dictionary<string, string> SwedishTranslations = new()
         {
             { "Hej", "Hello" },
             { "Ja", "Yes" },
@@ -15,7 +15,86 @@ namespace LanguageApp.Views
             { "Förlåt", "Sorry" },
             { "God morgon", "Good morning" },
             { "God natt", "Good night" },
-            { "Hej då", "Goodbye" }
+            { "Hej då", "Goodbye" },
+            { "Snälla", "Please" },
+            { "Ursäkta", "Excuse me" },
+            { "Hur mår du?", "How are you?" },
+            { "Jag förstår", "I understand" },
+            { "Jag vet inte", "I don't know" },
+            { "Vad heter du?", "What is your name?" },
+            { "Jag heter...", "My name is..." }
+        };
+        private readonly Dictionary<string, string> NorwegianTranslations = new()
+        {
+            { "Hei", "Hello" },
+            { "Ja", "Yes" },
+            { "Nei", "No" },
+            { "Takk", "Thank you" },
+            { "Beklager", "Sorry" },
+            { "God morgen", "Good morning" },
+            { "God natt", "Good night" },
+            { "Ha det", "Goodbye" },
+            { "Vær så snill", "Please" },
+            { "Unnskyld", "Excuse me" },
+            { "Hvordan har du det?", "How are you?" },
+            { "Jeg forstår", "I understand" },
+            { "Jeg vet ikke", "I don't know" },
+            { "Hva heter du?", "What is your name?" },
+            { "Jeg heter...", "My name is..." }
+        };
+        private readonly Dictionary<string, string> FinnishTranslations = new()
+        {
+            { "Hei", "Hello" },
+            { "Kyllä", "Yes" },
+            { "Ei", "No" },
+            { "Kiitos", "Thank you" },
+            { "Anteeksi", "Sorry" },
+            { "Hyvää huomenta", "Good morning" },
+            { "Hyvää yötä", "Good night" },
+            { "Hei hei", "Goodbye" },
+            { "Ole hyvä", "Please" },
+            { "Anteeksi, voisitko auttaa?", "Excuse me, could you help?" },
+            { "Mitä kuuluu?", "How are you?" },
+            { "Ymmärrän", "I understand" },
+            { "En tiedä", "I don't know" },
+            { "Mikä on nimesi?", "What is your name?" },
+            { "Minun nimeni on...", "My name is..." }
+        };
+        private readonly Dictionary<string, string> DanishTranslations = new()
+        {
+            { "Hej", "Hello" },
+            { "Ja", "Yes" },
+            { "Nej", "No" },
+            { "Tak", "Thank you" },
+            { "Undskyld", "Sorry" },
+            { "God morgen", "Good morning" },
+            { "God nat", "Good night" },
+            { "Farvel", "Goodbye" },
+            { "Vær venlig", "Please" },
+            { "Undskyld mig", "Excuse me" },
+            { "Hvordan har du det?", "How are you?" },
+            { "Jeg forstår", "I understand" },
+            { "Jeg ved ikke", "I don't know" },
+            { "Hvad hedder du?", "What is your name?" },
+            { "Jeg hedder...", "My name is..." }
+        };
+        private readonly Dictionary<string, string> IcelandicTranslations = new()
+        {
+            { "Halló", "Hello" },
+            { "Já", "Yes" },
+            { "Nei", "No" },
+            { "Takk", "Thank you" },
+            { "Fyrirgefðu", "Sorry" },
+            { "Góðan morgun", "Good morning" },
+            { "Góða nótt", "Good night" },
+            { "Bless", "Goodbye" },
+            { "Vinsamlegast", "Please" },
+            { "Afsakið", "Excuse me" },
+            { "Hvernig hefur þú það?", "How are you?" },
+            { "Ég skil", "I understand" },
+            { "Ég veit ekki", "I don't know" },
+            { "Hvað heitir þú?", "What is your name?" },
+            { "Ég heiti...", "My name is..." }
         };
 
         private readonly Random random = new();
@@ -29,17 +108,30 @@ namespace LanguageApp.Views
 
         private void LoadNextWord()
         {
-            
-            var keys = new List<string>(WordTranslations.Keys);
-            var randomKey = keys[random.Next(keys.Count)];
-            currentWord = new KeyValuePair<string, string>(randomKey, WordTranslations[randomKey]);
+            var currentLanguage = Preferences.Get("SelectedLanguage", "sv");
 
-            WordLabel.Text = currentWord.Key; 
+            Dictionary<string, string> wordTranslations = currentLanguage switch
+            {
+                "sv" => SwedishTranslations,
+                "no" => NorwegianTranslations,
+                "fi" => FinnishTranslations,
+                "da" => DanishTranslations,
+                "is" => IcelandicTranslations,
+                _ => SwedishTranslations
+            };
+
+            var keys = new List<string>(wordTranslations.Keys);
+            var randomKey = keys[random.Next(keys.Count)];
+            currentWord = new KeyValuePair<string, string>(randomKey, wordTranslations[randomKey]);
+
+            WordLabel.Text = currentWord.Key;
             FeedbackLabel.IsVisible = false;
             UserEntry.Text = string.Empty;
             NextWordButton.IsEnabled = false;
-            NextWordButton.Opacity =  0.5;
+            NextWordButton.Opacity = 0.5;
+
         }
+     
 
         private void OnSubmitClicked(object sender, EventArgs e)
         {
@@ -63,7 +155,7 @@ namespace LanguageApp.Views
             }
             else
             {
-                FeedbackLabel.Text = $"Incorrect !👎🙄. The correct translation is: {currentWord.Value}.";
+                FeedbackLabel.Text = $"Incorrect !👎. But that's okay! The correct translation is: {currentWord.Value}.";
                 FeedbackLabel.TextColor = Colors.Red;
                 FeedbackLabel.IsVisible = true;
                 NextWordButton.IsEnabled = true;
